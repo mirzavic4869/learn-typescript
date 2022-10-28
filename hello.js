770f8b6293bc72db4f759b4c2c433a381d76707b
mirzavic4869 <avicenamirzam@gmail.com>
2022-10-28 16:55:59 +0700

    say hello

---
+++ hello.js
@@ -0,0 +1,7 @@
+// function sayHello(name: string) {
+// 	return `Hello ${name}`
+// }
+// let response: string= sayHello(name:'Mirzam')
+// console.log(response);
+var sayHello = "Hello Mirzam Avicena";
+console.log(sayHello);
---
+++ hello.ts
@@ -0,0 +1,11 @@
+// function sayHello(name: string) {
+// 	return `Hello ${name}`
+// }
+
+// let response: string= sayHello(name:'Mirzam')
+
+// console.log(response);
+
+let sayHello: string = "Hello Mirzam Avicena";
+
+console.log(sayHello);
